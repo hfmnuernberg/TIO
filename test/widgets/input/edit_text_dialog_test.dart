49bd09401b6ab654dc0a5b893fab8f3b304072d6
Jerome Weiß <jerome@cultivate.team>
2025-02-03 12:42:13 +0100

    test: TIO-1: write missing tests

--- test/widgets/input/edit_text_dialog_test.dart
+++ test/widgets/input/edit_text_dialog_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tiomusic/widgets/input/edit_text_dialog.dart';
 
+final int maxLength = 100;
+
 extension WidgetTesterPumpExtension on WidgetTester {
   Future<void> renderWidget(Widget widget) => pumpWidget(MaterialApp(home: Scaffold(body: widget)));
 }
@@ -12,8 +14,8 @@ class TestWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: () => showEditTextDialog(context: context, label: 'Label', value: 'Old title'),
-        child: Text('Open Dialog'),
+      onPressed: () => showEditTextDialog(context: context, label: 'Label', value: 'Old title'),
+      child: Text('Open Dialog'),
     );
   }
 }
@@ -27,7 +29,7 @@ void main() {
     testWidgets('calls submit with edited text when save is pressed', (WidgetTester tester) async {
       String editedText = 'Never called';
       await tester.renderWidget(
-          EditTextDialog(label: "Label", value: "Old title", onSave: (text) => editedText = text, onCancel: () {}),
+        EditTextDialog(label: "Label", value: "Old title", onSave: (text) => editedText = text, onCancel: () {}),
       );
 
       await tester.enterText(find.bySemanticsLabel('Label'), "New title");
@@ -46,6 +48,29 @@ void main() {
       await tester.tap(find.bySemanticsLabel('Cancel'));
 
       expect(wasOnCancelCalled, true);
+    });
+
+    testWidgets('does not save new title when title has not changed', (WidgetTester tester) async {
+      var wasOnSaveCalled = false;
+      await tester.renderWidget(
+        EditTextDialog(label: "Label", value: "Old title", onSave: (_) => wasOnSaveCalled = true, onCancel: () {}),
+      );
+
+      await tester.tap(find.bySemanticsLabel('Submit'));
+
+      expect(wasOnSaveCalled, false);
+    });
+
+    testWidgets('does not save new title when new title is empty', (WidgetTester tester) async {
+      var wasOnSaveCalled = false;
+      await tester.renderWidget(
+        EditTextDialog(label: "Label", value: "Old title", onSave: (_) => wasOnSaveCalled = true, onCancel: () {}),
+      );
+
+      await tester.enterText(find.text('Old title'), '');
+      await tester.tap(find.bySemanticsLabel('Submit'));
+
+      expect(wasOnSaveCalled, false);
     });
 
     testWidgets('shows edit text dialog when open dialog is pressed', (WidgetTester tester) async {
@@ -68,6 +93,21 @@ void main() {
       await tester.tap(find.text('Cancel'));
       await tester.pump();
       expect(find.text('Old title'), findsNothing);
+    });
+
+    testWidgets('does not allow entering new title longer than max value', (WidgetTester tester) async {
+      await tester.renderWidget(TestWrapper());
+
+      await tester.tap(find.bySemanticsLabel('Open Dialog'));
+      await tester.pump();
+
+      expect(find.text('Old title'), findsOneWidget);
+
+      await tester.enterText(find.text('Old title'), 'a'.padLeft(maxLength + 1, 'a'));
+      await tester.pump();
+
+      final textField = tester.widget<TextField>(find.byType(TextField));
+      expect(textField.controller?.text.length, 100);
     });
   });
 }
