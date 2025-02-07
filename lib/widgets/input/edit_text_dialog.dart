ed23ceae47769071c7f55e198e2a0d28baf066d1
Jerome Weiß <jerome@cultivate.team>
2025-02-07 13:32:52 +0100

    fix: TIO-1: disallow clicking outside of widget and Android back button usage when text was edited but not saved

--- lib/widgets/input/edit_text_dialog.dart
+++ lib/widgets/input/edit_text_dialog.dart
@@ -7,23 +7,40 @@ Future<String?> showEditTextDialog({
   required String label,
   required String value,
   bool isNew = false,
-}) =>
-    showDialog<String>(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => EditTextDialog(
-        label: label,
-        value: value,
-        isNew: isNew,
-        onSave: (value) => Navigator.of(context).pop(value),
-        onCancel: () => Navigator.of(context).pop(),
-      ),
-    );
+}) {
+  final TextEditingController controller = TextEditingController(text: value);
+  controller.selection = TextSelection(baseOffset: 0, extentOffset: value.length);
+
+  return showDialog<String>(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return ValueListenableBuilder<TextEditingValue>(
+        valueListenable: controller,
+        builder: (context, textValue, child) {
+          return PopScope(
+            canPop: controller.text == value,
+            child: EditTextDialog(
+              label: label,
+              value: value,
+              isNew: isNew,
+              controller: controller,
+              onSave: (newValue) => Navigator.of(context).pop(newValue),
+              onCancel: () => Navigator.of(context).pop(),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+
 
 class EditTextDialog extends StatelessWidget {
   final String label;
   final String value;
   final bool isNew;
+  final TextEditingController controller;
   final Function(String value) onSave;
   final Function() onCancel;
 
@@ -32,62 +49,58 @@ class EditTextDialog extends StatelessWidget {
     required this.label,
     required this.value,
     this.isNew = false,
+    required this.controller,
     required this.onSave,
     required this.onCancel,
   });
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController(text: value);
-    controller.selection = TextSelection(baseOffset: 0, extentOffset: value.length);
-
     void handleSubmit() => onSave(controller.text);
 
-    return PopScope(
-      canPop: controller.text == value,
-      child: AlertDialog(
-        content: Transform.translate(
-          offset: const Offset(0, 10),
-          child: TextField(
-            autofocus: true,
-            maxLength: 100,
-            decoration: InputDecoration(
-              hintText: "",
-              border: OutlineInputBorder(borderSide: BorderSide(color: ColorTheme.primary)),
-              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: ColorTheme.primary)),
-              label: Text(label, style: TextStyle(color: ColorTheme.surfaceTint)),
-            ),
-            style: const TextStyle(color: ColorTheme.primary),
-            controller: controller,
-            onSubmitted: (_) => handleSubmit(),
+    return AlertDialog(
+      content: Transform.translate(
+        offset: const Offset(0, 10),
+        child: TextField(
+          autofocus: true,
+          maxLength: 100,
+          decoration: InputDecoration(
+            hintText: "",
+            border: OutlineInputBorder(borderSide: BorderSide(color: ColorTheme.primary)),
+            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: ColorTheme.primary)),
+            label: Text(label, style: TextStyle(color: ColorTheme.surfaceTint)),
           ),
+          style: const TextStyle(color: ColorTheme.primary),
+          controller: controller,
+          onSubmitted: (_) => handleSubmit(),
         ),
-        actions: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: onCancel,
-                child: const Text('Cancel'),
-              ),
-              ValueListenableBuilder<TextEditingValue>(
-                valueListenable: controller,
-                builder: (context, value, child) {
-                  final isValid = controller.text.isNotEmpty;
-                  final isDirty = controller.text != this.value;
-                  final isSubmitEnabled = isValid && (isNew || isDirty);
-
-                  return TIOFlatButton(
-                    onPressed: isSubmitEnabled ? handleSubmit : null,
-                    text: 'Submit',
-                    boldText: true,
-                  );
-                },
-              ),
-            ],
-          )
-        ],
       ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+              onPressed: onCancel,
+              child: const Text('Cancel'),
+            ),
+            ValueListenableBuilder<TextEditingValue>(
+              valueListenable: controller,
+              builder: (context, textValue, child) {
+                final isValid = controller.text.isNotEmpty;
+                final isDirty = controller.text != value;
+                final isSubmitEnabled = isValid && (isNew || isDirty);
+
+                return TIOFlatButton(
+                  onPressed: isSubmitEnabled ? handleSubmit : null,
+                  text: 'Submit',
+                  boldText: true,
+                );
+              },
+            ),
+          ],
+        )
+      ],
     );
   }
 }
+
