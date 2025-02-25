b97dcf5cdd1faeaadbe669f59a6b2f92cc9508a8
Jerome Weiß <jerome@cultivate.team>
2025-02-25 11:06:16 +0100

    fix: TIO-73: import project with media files

--- lib/pages/projects_list/import_project_dialog.dart
+++ lib/pages/projects_list/import_project_dialog.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:archive/archive.dart';
+import 'package:archive/archive_io.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:tiomusic/models/project.dart';
 import 'package:tiomusic/models/project_library.dart';
@@ -22,15 +25,46 @@ class ImportProjectDialog extends StatelessWidget {
   const ImportProjectDialog({super.key, required this.onDone});
 
   Future<File?> _getFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['json']);
+    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['.zip']);
 
     return result == null || result.files.single.path == null ? null : File(result.files.single.path!);
   }
 
-  Future<Project> _readProjectFromFile(File file) async {
-    String jsonString = await file.readAsString();
-    Map<String, dynamic> jsonData = jsonDecode(jsonString);
+  Future<Project> _readProjectFromArchiveFile(ArchiveFile archiveFile) async {
+    final jsonString = utf8.decode(archiveFile.content as List<int>);
+    final Map<String, dynamic> jsonData = jsonDecode(jsonString);
     return Project.fromJson(jsonData);
+  }
+
+  Future<Project?> _extractArchive(BuildContext context, File archiveFile) async {
+    final directory = await getApplicationDocumentsDirectory();
+    print('------ directory path: ${directory.path}');
+    final bytes = await archiveFile.readAsBytes();
+    final archive = ZipDecoder().decodeBytes(bytes);
+
+    Project? project = null;
+
+    for (final file in archive) {
+      print('------------------------------');
+      print('------ file name: ${file.name}');
+
+      if (file.name.endsWith('.json')) {
+        project = await _readProjectFromArchiveFile(file);
+        // context.read<ProjectLibrary>().addProject(project);
+        print('------ project: ${project.toJson()}');
+        continue;
+      }
+
+      print('------ is media file!');
+      final filePath = '${directory.path}/media/${file.name}';
+      print('------ media file path: $filePath');
+      final mediaFile = File(filePath);
+      print('------ new media file created!');
+      await mediaFile.writeAsBytes(file.content as List<int>);
+      print('------ media file written!');
+    }
+
+    return project;
   }
 
   Future<void> _importProject(BuildContext context) async {
@@ -39,17 +73,22 @@ class ImportProjectDialog extends StatelessWidget {
 
       if (file == null) {
         showSnackbar(context: context, message: 'No project file selected')();
-        onDone();
         return;
       }
 
-      final project = await _readProjectFromFile(file);
+      final project = await _extractArchive(context, file);
+
+      if (project == null) {
+        showSnackbar(context: context, message: 'Error importing project')();
+        return;
+      }
       context.read<ProjectLibrary>().addProject(project);
 
       showSnackbar(context: context, message: 'Project imported successfully!')();
-      onDone();
     } catch (_) {
       showSnackbar(context: context, message: 'Error importing project')();
+    } finally {
+      onDone();
     }
   }
 
