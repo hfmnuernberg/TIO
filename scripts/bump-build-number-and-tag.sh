9eb6e859e221f794e87e344659f16c7d36c91799
David Bieder <davidbieder@users.noreply.github.com>
2025-03-24 10:50:55 +0100

    ci: TIO-148: fix bug in bump build number script

--- scripts/bump-build-number-and-tag.sh
+++ scripts/bump-build-number-and-tag.sh
@@ -8,7 +8,7 @@ source ./scripts/load-build-number-from-latest-tag.sh
 
 NEW_BUILD_NUMBER=$((BUILD_NUMBER + 1))
 
-TAG="b$VERSION+$NEW_BUILD_NUMBER"
+TAG="b$NEW_BUILD_NUMBER"
 git tag "$TAG"
 git push origin "$TAG"
 
