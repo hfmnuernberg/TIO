7183a7dff259c06f0880eb679094b77004f098ac
Jerome Weiß <jerome@cultivate.team>
2025-03-28 13:02:20 +0100

    refactor: TIO-160: format code

--- lib/l10n/de.dart
+++ lib/l10n/de.dart
@@ -5,21 +5,26 @@ import 'package:tiomusic/l10n/app_localization.dart';
 
 class German extends AppLocalizations {
   String get appAboutDataProtection => 'Datenschutz';
-  String get appAboutDataProtectionExplanation => 'Wir erheben keine deiner Daten. Bitte beachte, dass deine Projekte nur lokal auf deinem Gerät gespeichert werden, d.h. sie werden nicht in der App oder in einem Cloud-Dienst o. Ä. gespeichert. Wenn du dich dazu entscheidest, einzelne Inhalte aus der App heraus zu teilen, ist dies über Drittanbieterdienste wie Messenger, etc. möglich. In solchen Fällen gelten ausschließlich die Datenschutzbestimmungen der verwendeten Drittanbieterdienste. Du selbst bist dafür verantwortlich, die geltenden Datenschutz- oder Urheberrechtsbestimmungen einzuhalten.';
+  String get appAboutDataProtectionExplanation =>
+      'Wir erheben keine deiner Daten. Bitte beachte, dass deine Projekte nur lokal auf deinem Gerät gespeichert werden, d.h. sie werden nicht in der App oder in einem Cloud-Dienst o. Ä. gespeichert. Wenn du dich dazu entscheidest, einzelne Inhalte aus der App heraus zu teilen, ist dies über Drittanbieterdienste wie Messenger, etc. möglich. In solchen Fällen gelten ausschließlich die Datenschutzbestimmungen der verwendeten Drittanbieterdienste. Du selbst bist dafür verantwortlich, die geltenden Datenschutz- oder Urheberrechtsbestimmungen einzuhalten.';
   String get appAboutDeveloperOne => 'Entwickler: cultivate(software)';
   String get appAboutDeveloperTwo => 'Entwickler: Studio Fluffy';
   String get appAboutEditor => 'Herausgeber: Hochschule für Musik Nürnberg';
   String get appAboutFeatures => 'Funktionen';
   String get appAboutImprint => 'Impressum';
-  String get appAboutParagraphOne => 'TIO Music integriert zahlreiche Tools (Stimmgerät, Metronom, Media Player, Klavier, Bildnotizen und Textnotizen) in einer App und ermöglicht die kombinierte Nutzung der einzelnen Tools. Durch das Erstellen von Projekten ist es möglich, verschiedene Konfigurationen zu speichern und so das Üben und Musizieren zu erleichtern. Die Tools können auch einzeln verwendet werden, z.B. für schnelles Stimmen von Instrumenten oder zum Aufnehmen von Proben. TIO Music wurde von Musikern für Musiker aller Erfahrungsstufen, für Anfänger und Profis entwickelt. Die App ist und bleibt vollständig kostenlos und werbefrei.';
-  String get appAboutParagraphThree => 'Diese App wurde im Rahmen des RE|LEVEL-Projekts an der Hochschule für Musik Nürnberg entwickelt. RE|LEVEL wird von der Stiftung Innovation in der Hochschullehre gefördert.';
-  String get appAboutParagraphTwo => 'Wir möchten die App kontinuierlich für dich verbessern - daher freuen wir uns auf dein Feedback!';
+  String get appAboutParagraphOne =>
+      'TIO Music integriert zahlreiche Tools (Stimmgerät, Metronom, Media Player, Klavier, Bildnotizen und Textnotizen) in einer App und ermöglicht die kombinierte Nutzung der einzelnen Tools. Durch das Erstellen von Projekten ist es möglich, verschiedene Konfigurationen zu speichern und so das Üben und Musizieren zu erleichtern. Die Tools können auch einzeln verwendet werden, z.B. für schnelles Stimmen von Instrumenten oder zum Aufnehmen von Proben. TIO Music wurde von Musikern für Musiker aller Erfahrungsstufen, für Anfänger und Profis entwickelt. Die App ist und bleibt vollständig kostenlos und werbefrei.';
+  String get appAboutParagraphThree =>
+      'Diese App wurde im Rahmen des RE|LEVEL-Projekts an der Hochschule für Musik Nürnberg entwickelt. RE|LEVEL wird von der Stiftung Innovation in der Hochschullehre gefördert.';
+  String get appAboutParagraphTwo =>
+      'Wir möchten die App kontinuierlich für dich verbessern - daher freuen wir uns auf dein Feedback!';
   String get appAboutTitle => 'Über TIO Music';
   String get appAboutVersion => 'App Version';
   String get appAboutVersionError => 'Konnte die App-Version nicht laden.';
   String get appTutorialQuickToolSave => 'Tippe hier um das Tool in einem Projekt zu speichern';
   String get appTutorialToolEditTitle => 'Tippe hier um den Titel deines Tools zu bearbeiten';
-  String get appTutorialToolIsland => 'Tippe hier um dein Tool mit einem Metronom, Stimmgerät oder Media Player zu kombinieren';
+  String get appTutorialToolIsland =>
+      'Tippe hier um dein Tool mit einem Metronom, Stimmgerät oder Media Player zu kombinieren';
   String get appTutorialToolSave => 'Tippe hier um dein Tool in ein anderes Projekt zu kopieren';
 
   String get commonCancel => 'Abbrechen';
@@ -39,29 +44,37 @@ class German extends AppLocalizations {
   String get homeFeedback => 'Feedback';
 
   String get imageAbout => 'Bild';
-  String get imageAboutExplanation => 'Du kannst Bilder oder Notenblätter über die Kamera deines Geräts in die App hochladen.';
+  String get imageAboutExplanation =>
+      'Du kannst Bilder oder Notenblätter über die Kamera deines Geräts in die App hochladen.';
 
   String get mediaPlayer => 'Media Player';
   String get mediaPlayerAbout => 'Media Player';
-  String get mediaPlayerAboutExplanation => 'Du kannst Audio-Dateien aufnehmen, laden und bearbeiten und Konfigurationen speichern. Dabei kannst du deine bevorzugte Lautstärke, Reichweite (Länge und Segment), Wiedergabegeschwindigkeit und Tonhöhe einstellen. Du kannst deine Projekte über externe Messenger-Dienste an andere weiterleiten.';
+  String get mediaPlayerAboutExplanation =>
+      'Du kannst Audio-Dateien aufnehmen, laden und bearbeiten und Konfigurationen speichern. Dabei kannst du deine bevorzugte Lautstärke, Reichweite (Länge und Segment), Wiedergabegeschwindigkeit und Tonhöhe einstellen. Du kannst deine Projekte über externe Messenger-Dienste an andere weiterleiten.';
   String get mediaPlayerTutorialAdjust => 'Tippe hier um deine Audiodatei anzupassen';
   String get mediaPlayerTutorialJumpTo => 'Tippe irgendwo um zu diesem Teil deiner Audiodatei zu springen';
-  String get mediaPlayerTutorialStartStop => 'Tippe hier um die Aufnahme zu starten und zu stoppen oder um eine Audiodatei abzuspielen';
+  String get mediaPlayerTutorialStartStop =>
+      'Tippe hier um die Aufnahme zu starten und zu stoppen oder um eine Audiodatei abzuspielen';
 
   String get metronome => 'Metronom';
   String get metronomeAbout => 'Metronom';
-  String get metronomeAboutExplanation => 'Das Metronom ermöglicht es dir, deine individuellen Konfigurationen (Tempo, Taktart, Polyrhythmen, zufälliges Stummschalten, Klänge) zu speichern und abzurufen. Du kannst das Metronom auch mit dem Stimmgerät und dem Media Player kombinieren.';
+  String get metronomeAboutExplanation =>
+      'Das Metronom ermöglicht es dir, deine individuellen Konfigurationen (Tempo, Taktart, Polyrhythmen, zufälliges Stummschalten, Klänge) zu speichern und abzurufen. Du kannst das Metronom auch mit dem Stimmgerät und dem Media Player kombinieren.';
   String get metronomeTutorialAddNew => 'Tippe hier um ein zweites Metronom hinzuzufügen';
   String get metronomeTutorialAdjust => 'Tippe hier um die Metronomeinstellungen anzupassen';
-  String get metronomeTutorialEditBeats => 'Tippe auf einen Schlag um zwischen akzentuiert, unakzentuiert und stumm zu wechseln';
-  String get metronomeTutorialRelocate => 'Halte und ziehe seitlich um zu verschieben,\nstreiche nach oben um zu löschen\noder tippe um zu bearbeiten';
+  String get metronomeTutorialEditBeats =>
+      'Tippe auf einen Schlag um zwischen akzentuiert, unakzentuiert und stumm zu wechseln';
+  String get metronomeTutorialRelocate =>
+      'Halte und ziehe seitlich um zu verschieben,\nstreiche nach oben um zu löschen\noder tippe um zu bearbeiten';
   String get metronomeTutorialStartStop => 'Tippe hier um das Metronom zu starten und zu stoppen';
 
   String get piano => 'Klavier';
   String get pianoAbout => 'Klavier';
-  String get pianoAboutExplanation => 'Du kannst das integrierte Klavier verwenden, verschiedene Klangmodi auswählen und deine individuellen Konfigurationen speichern.';
+  String get pianoAboutExplanation =>
+      'Du kannst das integrierte Klavier verwenden, verschiedene Klangmodi auswählen und deine individuellen Konfigurationen speichern.';
   String get pianoTutorialAdjust => 'Tippe hier um Konzerttonhöhe, Lautstärke und Klang anzupassen';
-  String get pianoTutorialChangeKeyOrOctave => 'Tippe auf die Pfeile links oder rechts um pro Taste oder pro Oktave nach oben oder unten zu wechseln';
+  String get pianoTutorialChangeKeyOrOctave =>
+      'Tippe auf die Pfeile links oder rechts um pro Taste oder pro Oktave nach oben oder unten zu wechseln';
 
   String get projectDeleteAllTools => 'Alle Tools löschen';
   String get projectDeleteAllToolsConfirmation => 'Möchtest du wirklich alle Tools in diesem Projekt löschen?';
@@ -76,7 +89,8 @@ class German extends AppLocalizations {
   String get projectTutorialEditTitle => 'Tippe hier um den Projekttitel zu bearbeiten';
 
   String get projectsAbout => 'Projekte';
-  String get projectsAboutExplanation => 'Du kannst Projekte erstellen und die benötigten Einstellungen dort speichern (Einstellungen für Instrumentenstimmung, Metronomeinstellungen, etc.), um sie jederzeit abrufen zu können.';
+  String get projectsAboutExplanation =>
+      'Du kannst Projekte erstellen und die benötigten Einstellungen dort speichern (Einstellungen für Instrumentenstimmung, Metronomeinstellungen, etc.), um sie jederzeit abrufen zu können.';
   String get projectsDeleteAll => 'Alle Projekte löschen';
   String get projectsDeleteAllConfirmation => 'Möchtest du wirklich alle Projekte löschen?';
   String get projectsDeleteConfirmation => 'Möchtest du dieses Projekt wirklich löschen?';
@@ -87,17 +101,21 @@ class German extends AppLocalizations {
   String get projectsNew => 'Neues Projekt';
   String get projectsNoProjects => 'Bitte klicke auf das "+" um ein neues Projekt zu erstellen.';
   String get projectsTutorialAddProject => 'Tippe hier um ein neues Projekt zu erstellen';
-  String get projectsTutorialCanIncludeMultipleTools => 'Projekte können mehrere Tools enthalten\n(Stimmgerät, Metronom, Klaviereinstellungen, Media Player, Bild und Text),\nsogar mehrere Tools des gleichen Typs.';
-  String get projectsTutorialHowToUseTio => 'Willkommen! Du kannst TIO auf zwei Arten verwenden.\n1. Erstelle ein Projekt und füge Tools hinzu.\n2. Starte mit der Verwendung eines Tools und speichere deine spezifischen Einstellungen in einem Projekt.';
+  String get projectsTutorialCanIncludeMultipleTools =>
+      'Projekte können mehrere Tools enthalten\n(Stimmgerät, Metronom, Klaviereinstellungen, Media Player, Bild und Text),\nsogar mehrere Tools des gleichen Typs.';
+  String get projectsTutorialHowToUseTio =>
+      'Willkommen! Du kannst TIO auf zwei Arten verwenden.\n1. Erstelle ein Projekt und füge Tools hinzu.\n2. Starte mit der Verwendung eines Tools und speichere deine spezifischen Einstellungen in einem Projekt.';
   String get projectsTutorialStart => 'Tutorial anzeigen';
   String get projectsTutorialStartUsingTool => 'Tippe hier um ein Tool zu verwenden';
 
   String get textAbout => 'Text';
-  String get textAboutExplanation => 'Du kannst über dein Gerät Textnotizen erstellen, z.B. für Spielanweisungen, Hintergrundinformationen, Songtexte, etc.';
+  String get textAboutExplanation =>
+      'Du kannst über dein Gerät Textnotizen erstellen, z.B. für Spielanweisungen, Hintergrundinformationen, Songtexte, etc.';
 
   String get tuner => 'Stimmgerät';
   String get tunerAbout => 'Stimmgerät';
-  String get tunerAboutExplanation => 'Du kannst deine Instrumente auf jede Konzerttonart stimmen, Referenztöne abspielen, deine individuelle Konfiguration speichern und das Stimmgerät mit dem Metronom und dem Media Player kombinieren.';
+  String get tunerAboutExplanation =>
+      'Du kannst deine Instrumente auf jede Konzerttonart stimmen, Referenztöne abspielen, deine individuelle Konfiguration speichern und das Stimmgerät mit dem Metronom und dem Media Player kombinieren.';
   String get tunerTutorialAdjust => 'Tippe hier um die Konzerttonhöhe anzupassen oder einen Referenzton abzuspielen';
   String get tunerTutorialStartStop => 'Tippe hier um das Stimmgerät zu starten und zu stoppen';
 }
--- lib/l10n/en.dart
+++ lib/l10n/en.dart
@@ -5,15 +5,19 @@ import 'package:tiomusic/l10n/app_localization.dart';
 
 class English extends AppLocalizations {
   String get appAboutDataProtection => 'Data protection';
-  String get appAboutDataProtectionExplanation => 'We do not collect any of your data. Please note that your projects are only saved locally on your device, i.e. they are not saved in the app or in any cloud service or similar. If you decide to share individual content from within the app, this is possible via third-party services such as messenger etc. In such cases, only the data protection regulations of the third-party services used apply. You yourself are responsible for complying with applicable data protection or copyright regulations.';
+  String get appAboutDataProtectionExplanation =>
+      'We do not collect any of your data. Please note that your projects are only saved locally on your device, i.e. they are not saved in the app or in any cloud service or similar. If you decide to share individual content from within the app, this is possible via third-party services such as messenger etc. In such cases, only the data protection regulations of the third-party services used apply. You yourself are responsible for complying with applicable data protection or copyright regulations.';
   String get appAboutDeveloperOne => 'Developer: cultivate(software)';
   String get appAboutDeveloperTwo => 'Developer: Studio Fluffy';
   String get appAboutEditor => 'Editor: University of Music Nuremberg';
   String get appAboutFeatures => 'Features';
   String get appAboutImprint => 'Imprint';
-  String get appAboutParagraphOne => 'TIO Music integrates numerous tools (tuner, metronome, media player, piano, image notes and text notes) in one app and enables the combined use of the individual tools. By creating projects, it is possible to save different configurations and thus make practicing and making music easier. The tools can also be used individually, for quick tuning of instruments or for recording samples. TIO Music was developed by musicians for musicians of all levels of experience, for amateurs and professionals. The app is and will remain completely free of charge and ad-free.';
-  String get appAboutParagraphThree => 'This app was developed as part of the RE|LEVEL-project at Hochschule für Musik Nürnberg. RE|LEVEL is funded by Stiftung Innovation in der Hochschullehre.';
-  String get appAboutParagraphTwo => 'We aim to continuously improve the app for you - so we look forward to your feedback!';
+  String get appAboutParagraphOne =>
+      'TIO Music integrates numerous tools (tuner, metronome, media player, piano, image notes and text notes) in one app and enables the combined use of the individual tools. By creating projects, it is possible to save different configurations and thus make practicing and making music easier. The tools can also be used individually, for quick tuning of instruments or for recording samples. TIO Music was developed by musicians for musicians of all levels of experience, for amateurs and professionals. The app is and will remain completely free of charge and ad-free.';
+  String get appAboutParagraphThree =>
+      'This app was developed as part of the RE|LEVEL-project at Hochschule für Musik Nürnberg. RE|LEVEL is funded by Stiftung Innovation in der Hochschullehre.';
+  String get appAboutParagraphTwo =>
+      'We aim to continuously improve the app for you - so we look forward to your feedback!';
   String get appAboutTitle => 'About';
   String get appAboutVersion => 'App Version';
   String get appAboutVersionError => 'Could not load app version.';
@@ -39,27 +43,32 @@ class English extends AppLocalizations {
   String get homeFeedback => 'Feedback';
 
   String get imageAbout => 'Image';
-  String get imageAboutExplanation => 'You can upload pictures or note sheets to the app using the camera on your device.';
+  String get imageAboutExplanation =>
+      'You can upload pictures or note sheets to the app using the camera on your device.';
 
   String get mediaPlayer => 'Media Player';
   String get mediaPlayerAbout => 'Media player';
-  String get mediaPlayerAboutExplanation => 'You can record, load and edit audio files and save configurations. In doing so, you can set your preferred volume, range (length and segment), playing speed and pitch. You can forward your projects to others using external messenger services.';
+  String get mediaPlayerAboutExplanation =>
+      'You can record, load and edit audio files and save configurations. In doing so, you can set your preferred volume, range (length and segment), playing speed and pitch. You can forward your projects to others using external messenger services.';
   String get mediaPlayerTutorialAdjust => 'Tap here to adjust your sound file';
   String get mediaPlayerTutorialJumpTo => 'Tap anywhere to jump to that part of your sound file';
   String get mediaPlayerTutorialStartStop => 'Tap here to start and stop recording or to play a sound file';
 
   String get metronome => 'Metronome';
   String get metronomeAbout => 'Metronome';
-  String get metronomeAboutExplanation => 'The metronome allows you to save and recall your individual configurations (tempo, time signature, polyrhythms, random mute, sounds). You can also combine the metronome with the tuner and the media player.';
+  String get metronomeAboutExplanation =>
+      'The metronome allows you to save and recall your individual configurations (tempo, time signature, polyrhythms, random mute, sounds). You can also combine the metronome with the tuner and the media player.';
   String get metronomeTutorialAddNew => 'Tap here to add a second metronome';
   String get metronomeTutorialAdjust => 'Tap here to adjust the metronome settings';
   String get metronomeTutorialEditBeats => 'Tap a beat to switch between accented, unaccented and muted';
-  String get metronomeTutorialRelocate => 'Hold and drag sideways to relocate,\nswipe upwards to delete\nor tap to edit';
+  String get metronomeTutorialRelocate =>
+      'Hold and drag sideways to relocate,\nswipe upwards to delete\nor tap to edit';
   String get metronomeTutorialStartStop => 'Tap here to start and stop the metronome';
 
   String get piano => 'Piano';
   String get pianoAbout => 'Piano';
-  String get pianoAboutExplanation => 'You can use the built-in piano, select different sound modes and save your individual configurations.';
+  String get pianoAboutExplanation =>
+      'You can use the built-in piano, select different sound modes and save your individual configurations.';
   String get pianoTutorialAdjust => 'Tap here to adjust concert pitch, volume, and sound';
   String get pianoTutorialChangeKeyOrOctave => 'Tap the left or right arrows to move up or down per key or per octave';
 
@@ -76,7 +85,8 @@ class English extends AppLocalizations {
   String get projectTutorialEditTitle => 'Tap here to edit the title of your project';
 
   String get projectsAbout => 'Projects';
-  String get projectsAboutExplanation => 'You can create projects and save the required settings there (settings for instrument tuning, metronome settings, etc.), so you can access them whenever you want.';
+  String get projectsAboutExplanation =>
+      'You can create projects and save the required settings there (settings for instrument tuning, metronome settings, etc.), so you can access them whenever you want.';
   String get projectsDeleteAll => 'Delete all Projects';
   String get projectsDeleteAllConfirmation => 'Do you really want to delete all projects?';
   String get projectsDeleteConfirmation => 'Do you really want to delete this project?';
@@ -87,17 +97,21 @@ class English extends AppLocalizations {
   String get projectsNew => 'New Project';
   String get projectsNoProjects => 'Please click on "+" to create a new project.';
   String get projectsTutorialAddProject => 'Tap here to create a new project';
-  String get projectsTutorialCanIncludeMultipleTools => 'Projects can include multiple tools\n(tuner, metronome, piano setting, media player, image and text),\neven several tools of the same type.';
-  String get projectsTutorialHowToUseTio => 'Welcome! You can use TIO in two ways.\n1. Create a project and add tools.\n2. Start with using a tool and save your specific settings to any project.';
+  String get projectsTutorialCanIncludeMultipleTools =>
+      'Projects can include multiple tools\n(tuner, metronome, piano setting, media player, image and text),\neven several tools of the same type.';
+  String get projectsTutorialHowToUseTio =>
+      'Welcome! You can use TIO in two ways.\n1. Create a project and add tools.\n2. Start with using a tool and save your specific settings to any project.';
   String get projectsTutorialStart => 'Show Tutorial';
   String get projectsTutorialStartUsingTool => 'Tap here to start using a tool';
 
   String get textAbout => 'Text';
-  String get textAboutExplanation => 'You can use your device to create text notes, e.g. for playing instructions, background information, song lyrics etc.';
+  String get textAboutExplanation =>
+      'You can use your device to create text notes, e.g. for playing instructions, background information, song lyrics etc.';
 
   String get tuner => 'Tuner';
   String get tunerAbout => 'Tuner';
-  String get tunerAboutExplanation => 'You can tune your instruments to any concert pitch, play reference tones, save your individual configuration and combine the tuner with the metronome and media player.';
+  String get tunerAboutExplanation =>
+      'You can tune your instruments to any concert pitch, play reference tones, save your individual configuration and combine the tuner with the metronome and media player.';
   String get tunerTutorialAdjust => 'Tap here to adjust the concert pitch or play a reference tone';
   String get tunerTutorialStartStop => 'Tap here to start and stop the tuner';
 }
