7fb32a6cb261b715375f6caf78c62498f097fe6c
Jerome Weiß <jerome@cultivate.team>
2025-04-16 16:17:56 +0200

    refactor: TIO-172: refactor number input int to new improved version

---
+++ lib/widgets/input/number_input.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:tiomusic/widgets/input/number_input_dec.dart';
+
+class NumberInput extends StatelessWidget {
+  final int value;
+  final ValueChanged<int> onChanged;
+  final int? min;
+  final int? max;
+  final int? defaultValue;
+  final int? step;
+  final int? stepIntervalInMs;
+  final String? label;
+  final double? buttonRadius;
+  final double? buttonGap;
+  final double? textFieldWidth;
+  final double? textFontSize;
+  final double? relIconSize;
+
+  const NumberInput({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    this.min = 0,
+    this.max = 100,
+    this.defaultValue = 1,
+    this.step = 1,
+    this.stepIntervalInMs = 100,
+    this.label = '',
+    this.buttonRadius = 25,
+    this.buttonGap = 10,
+    this.textFieldWidth = 100,
+    this.textFontSize = 40,
+    this.relIconSize = 0.4,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NumberInputDec(
+      value: value.toDouble(),
+      onChanged: (val) => onChanged(val.toInt()),
+      min: min?.toDouble(),
+      max: max?.toDouble(),
+      defaultValue: defaultValue?.toDouble(),
+      step: step?.toDouble(),
+      stepIntervalInMs: stepIntervalInMs,
+      label: label,
+      buttonRadius: buttonRadius,
+      buttonGap: buttonGap,
+      textFieldWidth: textFieldWidth,
+      textFontSize: textFontSize,
+      relIconSize: relIconSize,
+    );
+  }
+}
