5770d9f3bf275d710650dd3194b4e4fe7cd48bae
Jerome Weiß <jerome@cultivate.team>
2025-05-22 15:16:35 +0200

    refactor: TIO-97: format code

--- lib/widgets/rhythm_preset.dart
+++ lib/widgets/rhythm_preset.dart
@@ -17,35 +17,30 @@ RhythmPreset getPresetRhythmPattern(String noteKey) {
         polyBeats: [],
         noteKey: NoteValues.quarter,
       );
-
     case NoteValues.eighth:
       return RhythmPreset(
         beats: [BeatType.Accented, BeatType.Unaccented, BeatType.Accented, BeatType.Unaccented],
         polyBeats: [],
         noteKey: NoteValues.eighth,
       );
-
     case NoteValues.sixteenth:
       return RhythmPreset(
         beats: List.generate(8, (i) => i % 4 == 0 ? BeatType.Accented : BeatType.Unaccented),
         polyBeats: [],
         noteKey: NoteValues.sixteenth,
       );
-
     case NoteValues.eighthDotted:
       return RhythmPreset(
         beats: [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented],
         polyBeats: [],
         noteKey: NoteValues.eighthDotted,
       );
-
     case NoteValues.tuplet3Quarter:
       return RhythmPreset(
         beats: [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented],
         polyBeats: [],
         noteKey: NoteValues.tuplet3Quarter,
       );
-
     default:
       return RhythmPreset(
         beats: [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented],
