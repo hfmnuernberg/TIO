928db47199008c4591111257f7039a1a12ab58ad
David Bieder <davidbieder@users.noreply.github.com>
2025-05-28 14:53:40 +0200

    ci: TIO-000: improve tagging and releasing (#303)
    **Are there specific parts that the reviewer should look out for?**
    
    - [x] no
    - [ ] yes (please specify)
    
    **Any other comments?** _(e.g., unrelated minor changes piggybacking
    this PR)_
    
    - [x] no
    - [ ] yes (please specify)
    
    **Helpful screenshots?**
    
    - [x] no
    - [ ] yes
    
    Co-authored-by: GitHub Actions <github-actions@github.com>

--- scripts/load-semver-from-pr-title.sh
+++ scripts/load-semver-from-pr-title.sh
@@ -14,10 +14,10 @@ echo "OLD_MAJOR=$OLD_MAJOR";     export OLD_MAJOR
 echo "OLD_MINOR=$OLD_MINOR";     export OLD_MINOR
 echo "OLD_PATCH=$OLD_PATCH";     export OLD_PATCH
 
-NEW_VERSION=$(echo "$PR_TITLE" | sed -n 's/.* to \([^ ]*\).*/\1/p')
-IFS='.' read -r NEW_MAJOR NEW_MINOR NEW_PATCH <<< "$NEW_VERSION"
+NEW_SEMVER=$(echo "$PR_TITLE" | sed -n 's/.* to \([^ ]*\).*/\1/p')
+IFS='.' read -r NEW_MAJOR NEW_MINOR NEW_PATCH <<< "$NEW_SEMVER"
 echo
-echo "NEW_VERSION=$NEW_VERSION"; export NEW_VERSION
+echo "NEW_SEMVER=$NEW_SEMVER";   export NEW_SEMVER
 echo "NEW_MAJOR=$NEW_MAJOR";     export NEW_MAJOR
 echo "NEW_MINOR=$NEW_MINOR";     export NEW_MINOR
 echo "NEW_PATCH=$NEW_PATCH";     export NEW_PATCH
