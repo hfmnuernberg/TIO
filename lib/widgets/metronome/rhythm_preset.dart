4ddc43d6f844031597395f88018daccd1ac5bbd5
Jerome Weiß <jerome@cultivate.team>
2025-05-30 16:57:04 +0200

    refactor: TIO-97: make lists unmodifiable to prevent overwriting values

--- lib/widgets/metronome/rhythm_preset.dart
+++ lib/widgets/metronome/rhythm_preset.dart
@@ -19,35 +19,35 @@ class RhythmPreset extends Equatable {
   const RhythmPreset({required this.beats, required this.polyBeats, required this.noteKey});
 
   static final RhythmPreset oneFourth = RhythmPreset(
-    beats: const [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented],
-    polyBeats: const [],
+    beats: List.unmodifiable([BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented]),
+    polyBeats: List.unmodifiable([]),
     noteKey: NoteValues.quarter,
   );
 
   static final RhythmPreset twoEighth = RhythmPreset(
-    beats: const [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented],
-    polyBeats: _repeatPolyBeatPattern(4, [BeatTypePoly.Muted, BeatTypePoly.Unaccented]),
+    beats: List.unmodifiable([BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented]),
+    polyBeats: List.unmodifiable(_repeatPolyBeatPattern(4, [BeatTypePoly.Muted, BeatTypePoly.Unaccented])),
     noteKey: NoteValues.quarter,
   );
 
   static final RhythmPreset fourSixteenth = RhythmPreset(
-    beats: const [BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented],
-    polyBeats: _repeatPolyBeatPattern(4, [
-      BeatTypePoly.Muted,
-      BeatTypePoly.Unaccented,
-      BeatTypePoly.Unaccented,
-      BeatTypePoly.Unaccented,
-    ]),
+    beats: List.unmodifiable([BeatType.Accented, BeatType.Unaccented, BeatType.Unaccented, BeatType.Unaccented]),
+    polyBeats: List.unmodifiable(
+      _repeatPolyBeatPattern(4, [
+        BeatTypePoly.Muted,
+        BeatTypePoly.Unaccented,
+        BeatTypePoly.Unaccented,
+        BeatTypePoly.Unaccented,
+      ]),
+    ),
     noteKey: NoteValues.quarter,
   );
 
   static RhythmPreset fromKey(RhythmPresetKey key) => switch (key) {
-    RhythmPresetKey.oneFourth => oneFourth.copy(),
-    RhythmPresetKey.twoEighth => twoEighth.copy(),
-    RhythmPresetKey.fourSixteenth => fourSixteenth.copy(),
+    RhythmPresetKey.oneFourth => oneFourth,
+    RhythmPresetKey.twoEighth => twoEighth,
+    RhythmPresetKey.fourSixteenth => fourSixteenth,
   };
-
-  RhythmPreset copy() => RhythmPreset(beats: List.from(beats), polyBeats: List.from(polyBeats), noteKey: noteKey);
 
   @override
   List<Object> get props => [beats, polyBeats, noteKey];
