37d427dc17c4eb467bf39c59c016f46c57d39372
Jerome Weiß <59569084+jeromeweiss@users.noreply.github.com>
2025-06-05 13:37:21 +0200

    fix: TIO-183: tutorial for media player basic beat (#320)
    **Are there specific parts that the reviewer should look out for?**
    
    - [x] no
    - [ ] yes (please specify)
    
    **Any other comments?** _(e.g., unrelated minor changes piggybacking
    this PR)_
    
    - [x] no
    - [ ] yes (please specify)
    
    **Helpful screenshots?**
    
    - [ ] no
    - [x] yes
    <img width="508" alt="image"
    src="https://github.com/user-attachments/assets/245a3720-38a8-4a98-8d24-771a23ceb3b9"
    />
    <img width="508" alt="image"
    src="https://github.com/user-attachments/assets/a2117bb2-78f0-4c17-a117-e8d40b9474ca"
    />
    
    ---------
    
    Co-authored-by: Jerome Weiß <jerome@cultivate.team>

--- lib/pages/projects_page/projects_page.dart
+++ lib/pages/projects_page/projects_page.dart
@@ -95,9 +95,9 @@ class _ProjectsPageState extends State<ProjectsPage> {
       CustomTargetFocus(
         _keyNavigationBar,
         context.l10n.projectsTutorialStartUsingTool,
-        buttonsPosition: ButtonsPosition.top,
-        pointingDirection: PointingDirection.down,
         alignText: ContentAlign.top,
+        pointingDirection: PointingDirection.down,
+        buttonsPosition: ButtonsPosition.top,
         shape: ShapeLightFocus.RRect,
       ),
       CustomTargetFocus(
