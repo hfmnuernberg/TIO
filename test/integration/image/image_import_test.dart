2ddfd3072685c0738f5dde448bb41095d5c374e7
Jerome Weiß <jerome@cultivate.team>
2025-06-10 11:01:20 +0200

    test: TIO-122: test image import [WIP]

---
+++ test/integration/image/image_import_test.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:tiomusic/models/blocks/image_block.dart';
+import 'package:tiomusic/models/project.dart';
+import 'package:tiomusic/models/project_library.dart';
+import 'package:tiomusic/pages/image/image_page.dart';
+import 'package:tiomusic/pages/project_page/project_page.dart';
+import 'package:tiomusic/services/decorators/file_picker_log_decorator.dart';
+import 'package:tiomusic/services/decorators/file_references_log_decorator.dart';
+import 'package:tiomusic/services/decorators/file_system_log_decorator.dart';
+import 'package:tiomusic/services/decorators/media_repository_log_decorator.dart';
+import 'package:tiomusic/services/decorators/project_repository_log_decorator.dart';
+import 'package:tiomusic/services/file_picker.dart';
+import 'package:tiomusic/services/file_references.dart';
+import 'package:tiomusic/services/file_system.dart';
+import 'package:tiomusic/services/impl/file_based_media_repository.dart';
+import 'package:tiomusic/services/impl/file_based_project_repository.dart';
+import 'package:tiomusic/services/impl/file_references_impl.dart';
+import 'package:tiomusic/services/media_repository.dart';
+import 'package:tiomusic/services/project_repository.dart';
+
+import '../../mocks/file_picker_mock.dart';
+import '../../mocks/in_memory_file_system_mock.dart';
+import '../../utils/action_utils.dart';
+import '../../utils/project_utils.dart';
+import '../../utils/render_utils.dart';
+
+void main() {
+  late FileSystem fileSystem;
+  late FilePickerMock filePickerMock;
+  late List<SingleChildWidget> providers;
+
+  setUpAll(WidgetsFlutterBinding.ensureInitialized);
+
+  setUp(() async {
+    fileSystem = FileSystemLogDecorator(InMemoryFileSystemMock());
+    filePickerMock = FilePickerMock(fileSystem);
+    final filePicker = FilePickerLogDecorator(filePickerMock);
+    final mediaRepo = MediaRepositoryLogDecorator(FileBasedMediaRepository(fileSystem));
+    final projectRepo = ProjectRepositoryLogDecorator(FileBasedProjectRepository(fileSystem));
+    final fileReferences = FileReferencesLogDecorator(FileReferencesImpl(mediaRepo));
+
+    await fileSystem.init();
+    await mediaRepo.init();
+    final projectLibrary =
+        projectRepo.existsLibrary() ? await projectRepo.loadLibrary() : ProjectLibrary.withDefaults()
+          ..dismissAllTutorials();
+    await projectRepo.saveLibrary(projectLibrary);
+    await fileReferences.init(projectLibrary);
+    final project = Project.defaultThumbnail('Test Project');
+    // final image = ImageBlock.withTitle('Test Image');
+
+    providers = [
+      Provider<FilePicker>(create: (_) => filePicker),
+      Provider<FileSystem>(create: (_) => fileSystem),
+      Provider<MediaRepository>(create: (_) => mediaRepo),
+      Provider<ProjectRepository>(create: (_) => projectRepo),
+      Provider<FileReferences>(create: (_) => fileReferences),
+      ChangeNotifierProvider<ProjectLibrary>.value(value: projectLibrary),
+      ChangeNotifierProvider<Project>.value(value: project),
+      // ChangeNotifierProvider<ImageBlock>.value(value: image),
+    ];
+  });
+
+  group('ImageTool', () {
+    testWidgets('uploads image', (tester) async {
+      const imagePath = 'assets/test/black_circle.jpg';
+      filePickerMock.mockShareFileAndCapture(imagePath);
+      filePickerMock.mockPickArchive(imagePath);
+
+      await tester.renderScaffold(ProjectPage(goStraightToTool: false, withoutRealProject: false), providers);
+
+      // await tester.renderScaffold(ImageTool(isQuickTool: false), providers);
+      // expect(find.bySemanticsLabel('Test Image'), findsOneWidget);
+
+      await tester.createImageToolInProject();
+      await tester.tapAndSettle(find.bySemanticsLabel('Image 1'));
+      expect(find.bySemanticsLabel('Please select an image or take a photo.'), findsOneWidget);
+
+      debugDumpSemanticsTree();
+      await tester.tapAndSettle(find.bySemanticsLabel('Pick image(s)'));
+
+      // return mock file picker result
+
+      // expect(find.bySemanticsLabel('Image 1'), findsOneWidget);
+    });
+  });
+}
