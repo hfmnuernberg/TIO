f1f862abdd05e819cb0b424adf43a1b81fa10198
Jerome Weiß <59569084+jeromeweiss@users.noreply.github.com>
2025-06-12 18:34:16 +0200

    fix: TIO-97: minor improvements (#335)
    **Are there specific parts that the reviewer should look out for?**
    
    - [x] no
    - [ ] yes (please specify)
    
    **Any other comments?** _(e.g., unrelated minor changes piggybacking
    this PR)_
    
    - [x] no
    - [ ] yes (please specify)
    
    **Helpful screenshots?**
    
    - [x] no
    - [ ] yes
    
    ---------
    
    Co-authored-by: Jerome Weiß <jerome@cultivate.team>

--- lib/models/rhythm.dart
+++ lib/models/rhythm.dart
@@ -2,10 +2,10 @@ enum Rhythm {
   quarter('quarter', true, []),
   eighths('eighth', true, [false, true]),
   eighthRestFollowedByEighth('eighth_rest_followed_by_eighth', false, [false, true]),
-  triplets('triplets', true, [false, true, true]),
-  sixteenths('sixteenths', true, [false, true, true, true]),
+  dottedEighthFollowedBySixteenth('dotted_eighth_followed_by_sixteenth', true, [false, false, false, true]),
   sixteenthFollowedByDottedEighth('sixteenth_followed_by_dotted_eighth', true, [false, true, false, false]),
-  dottedEighthFollowedBySixteenth('dotted_eighth_followed_by_sixteenth', true, [false, false, false, true]);
+  triplets('triplets', true, [false, true, true]),
+  sixteenths('sixteenths', true, [false, true, true, true]);
 
   final String assetName;
   final bool main;
