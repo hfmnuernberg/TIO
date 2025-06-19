cec5cb94730246a13958241496be9853dc675aae
Jerome Weiß <jerome@cultivate.team>
2025-06-19 12:49:46 +0200

    refactor: TIO-226: rename methods; create select method

--- lib/pages/tuner/tuner_type_page.dart
+++ lib/pages/tuner/tuner_type_page.dart
@@ -17,27 +17,35 @@ class TunerTypePage extends StatefulWidget {
 }
 
 class _TunerTypePageState extends State<TunerTypePage> {
-  late TunerBlock _tunerBlock;
-  final List<bool> _selectedTuners = List.filled(TunerType.values.length, false);
+  late TunerBlock tunerBlock;
+  final List<bool> selectedTuners = List.filled(TunerType.values.length, false);
 
   @override
   void initState() {
     super.initState();
-    _tunerBlock = Provider.of<ProjectBlock>(context, listen: false) as TunerBlock;
-    _selectedTuners[_tunerBlock.tunerType.index] = true;
+    tunerBlock = Provider.of<ProjectBlock>(context, listen: false) as TunerBlock;
+    selectedTuners[tunerBlock.tunerType.index] = true;
   }
 
-  Future<void> _onConfirm() async {
-    final selectedIndex = _selectedTuners.indexWhere((element) => element);
-    _tunerBlock.tunerType = TunerType.values[selectedIndex];
+  void handleSelect(int index) {
+    setState(() {
+      for (int i = 0; i < selectedTuners.length; i++) {
+        selectedTuners[i] = i == index;
+      }
+    });
+  }
+
+  Future<void> handleConfirm() async {
+    final selectedIndex = selectedTuners.indexWhere((element) => element);
+    tunerBlock.tunerType = TunerType.values[selectedIndex];
     await context.read<ProjectRepository>().saveLibrary(context.read<ProjectLibrary>());
     if (!mounted) return;
     Navigator.pop(context);
   }
 
-  void _reset() {
-    for (int i = 0; i < _selectedTuners.length; i++) {
-      _selectedTuners[i] = i == TunerType.chromatic.index;
+  void handleReset() {
+    for (int i = 0; i < selectedTuners.length; i++) {
+      selectedTuners[i] = i == TunerType.chromatic.index;
     }
     setState(() {});
   }
@@ -46,20 +54,14 @@ class _TunerTypePageState extends State<TunerTypePage> {
   Widget build(BuildContext context) {
     return ParentSettingPage(
       title: context.l10n.tunerTypeSelect,
-      confirm: _onConfirm,
-      reset: _reset,
+      confirm: handleConfirm,
+      reset: handleReset,
       customWidget: Center(
         child: ToggleButtons(
           direction: Axis.vertical,
           constraints: const BoxConstraints(minHeight: 30, minWidth: 200),
-          onPressed: (index) {
-            setState(() {
-              for (int i = 0; i < _selectedTuners.length; i++) {
-                _selectedTuners[i] = i == index;
-              }
-            });
-          },
-          isSelected: _selectedTuners,
+          onPressed: handleSelect,
+          isSelected: selectedTuners,
           children:
               TunerType.values
                   .map((type) => Text(type.getLabel(context.l10n), style: const TextStyle(color: ColorTheme.primary)))
