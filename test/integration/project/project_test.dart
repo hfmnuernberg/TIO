eb4fb681ca8449ba0a171507bb0580929e061a7d
Jerome Weiß <jerome@cultivate.team>
2025-07-07 17:33:42 +0200

    refactor: TIO-226: remove redundant setup after deleting test

--- test/integration/project/project_test.dart
+++ test/integration/project/project_test.dart
@@ -5,8 +5,6 @@ import 'package:provider/single_child_widget.dart';
 import 'package:tiomusic/models/project.dart';
 import 'package:tiomusic/models/project_library.dart';
 import 'package:tiomusic/pages/project_page/project_page.dart';
-import 'package:tiomusic/services/audio_system.dart';
-import 'package:tiomusic/services/decorators/audio_system_log_decorator.dart';
 import 'package:tiomusic/services/decorators/file_picker_log_decorator.dart';
 import 'package:tiomusic/services/decorators/file_references_log_decorator.dart';
 import 'package:tiomusic/services/decorators/file_system_log_decorator.dart';
@@ -21,7 +19,6 @@ import 'package:tiomusic/services/impl/file_references_impl.dart';
 import 'package:tiomusic/services/media_repository.dart';
 import 'package:tiomusic/services/project_repository.dart';
 
-import '../../mocks/audio_system_mock.dart';
 import '../../mocks/file_picker_mock.dart';
 import '../../mocks/in_memory_file_system_mock.dart';
 import '../../utils/action_utils.dart';
@@ -37,7 +34,6 @@ extension WidgetTesterPumpExtension on WidgetTester {
 }
 
 void main() {
-  late AudioSystem audioSystemMock;
   late FileSystem inMemoryFileSystem;
   late FilePickerMock filePickerMock;
   late List<SingleChildWidget> providers;
@@ -45,11 +41,9 @@ void main() {
   setUpAll(WidgetsFlutterBinding.ensureInitialized);
 
   setUp(() async {
-    audioSystemMock = AudioSystemMock();
     inMemoryFileSystem = FileSystemLogDecorator(InMemoryFileSystemMock());
     filePickerMock = FilePickerMock(inMemoryFileSystem);
 
-    final audioSystem = AudioSystemLogDecorator(audioSystemMock);
     final filePicker = FilePickerLogDecorator(filePickerMock);
     final mediaRepo = MediaRepositoryLogDecorator(FileBasedMediaRepository(inMemoryFileSystem));
     final projectRepo = ProjectRepositoryLogDecorator(FileBasedProjectRepository(inMemoryFileSystem));
@@ -65,7 +59,6 @@ void main() {
     final project = Project.defaultThumbnail('Test Project');
 
     providers = [
-      Provider<AudioSystem>(create: (_) => audioSystem),
       Provider<FilePicker>(create: (_) => filePicker),
       Provider<FileSystem>(create: (_) => inMemoryFileSystem),
       Provider<MediaRepository>(create: (_) => mediaRepo),
