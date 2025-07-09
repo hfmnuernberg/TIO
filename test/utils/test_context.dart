36ede73d372d9a019a7db40417126c7dc95e7e28
Jerome Weiß <jerome@cultivate.team>
2025-07-09 09:39:59 +0200

    test: TIO-228: support archiver to shorten test setup

--- test/utils/test_context.dart
+++ test/utils/test_context.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:tiomusic/models/project.dart';
 import 'package:tiomusic/models/project_library.dart';
+import 'package:tiomusic/services/archiver.dart';
 import 'package:tiomusic/services/audio_system.dart';
+import 'package:tiomusic/services/decorators/archiver_log_decorator.dart';
 import 'package:tiomusic/services/decorators/audio_system_log_decorator.dart';
 import 'package:tiomusic/services/decorators/file_picker_log_decorator.dart';
 import 'package:tiomusic/services/decorators/file_references_log_decorator.dart';
@@ -13,6 +15,7 @@ import 'package:tiomusic/services/decorators/project_repository_log_decorator.da
 import 'package:tiomusic/services/file_picker.dart';
 import 'package:tiomusic/services/file_references.dart';
 import 'package:tiomusic/services/file_system.dart';
+import 'package:tiomusic/services/impl/file_based_archiver.dart';
 import 'package:tiomusic/services/impl/file_based_media_repository.dart';
 import 'package:tiomusic/services/impl/file_based_project_repository.dart';
 import 'package:tiomusic/services/impl/file_references_impl.dart';
@@ -34,6 +37,8 @@ class TestContext {
 
   late final mediaRepo = MediaRepositoryLogDecorator(FileBasedMediaRepository(inMemoryFileSystem));
   late final fileReferences = FileReferencesLogDecorator(FileReferencesImpl(mediaRepo));
+
+  late final archiver = ArchiverLogDecorator(FileBasedArchiver(inMemoryFileSystem, mediaRepo));
 
   late final projectRepo = ProjectRepositoryLogDecorator(FileBasedProjectRepository(inMemoryFileSystem));
 
@@ -59,6 +64,7 @@ class TestContext {
       Provider<MediaRepository>(create: (_) => mediaRepo),
       Provider<ProjectRepository>(create: (_) => projectRepo),
       Provider<FileReferences>(create: (_) => fileReferences),
+      Provider<Archiver>(create: (_) => archiver),
       ChangeNotifierProvider<ProjectLibrary>.value(value: projectLibrary),
       if (project != null)
         ChangeNotifierProvider<Project>.value(value: project),
