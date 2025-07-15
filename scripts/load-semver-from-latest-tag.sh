da97b51b295d4f54ffbe297fc8f5742e2b496e8d
David Bieder <davidbieder@users.noreply.github.com>
2025-07-15 12:13:36 +0200

    chore: TIO-000: use correct script to load semantic version (#374)
    **Are there specific parts that the reviewer should look out for?**
    
    - [x] no
    - [ ] yes (please specify)
    
    **Any other comments?** _(e.g., unrelated minor changes piggybacking
    this PR)_
    
    - [x] no
    - [ ] yes (please specify)
    
    **Helpful screenshots?**
    
    - [x] no
    - [ ] yes
    
    ---------
    
    Co-authored-by: GitHub Actions <github-actions@github.com>

--- scripts/load-semver-from-latest-tag.sh
+++ scripts/load-semver-from-latest-tag.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "⚙️ Loading version from last git tag..."
+echo "⚙️ Loading semantic version from latest Git tag..."
 
 if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   echo "❌  Not inside a Git repository" >&2
@@ -14,17 +14,17 @@ TAG=$(git tag --list 'v*' --sort=-creatordate | head -n 1)
 set -e
 
 if [ -z "$TAG" ]; then
-  echo "⚠️️ No Git tag found! Assuming version 0.0.1."
-  VERSION="0.0.1"
+  echo "⚠️️ No Git tag found! Assuming semantic version 0.0.1."
+  SEMVER="0.0.1"
 else
-  VERSION=$(echo "$TAG" | sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-  if [ "$VERSION" = "$TAG" ]; then
-    echo "⚠️️ No version found! Assuming version 0.0.1."
-    VERSION="0.0.1"
+  SEMVER=$(echo "$TAG" | sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+  if [ "$SEMVER" = "$TAG" ]; then
+    echo "⚠️️ No semantic version found in Git tag! Assuming semantic version 0.0.1."
+    SEMVER="0.0.1"
   fi
 fi
 
-export VERSION
+export SEMVER
 
-echo "VERSION=$VERSION"
-echo "✅️ Loaded version from last git tag."
+echo "SEMVER=$SEMVER"
+echo "✅️ Loaded semantic version from latest Git tag."
