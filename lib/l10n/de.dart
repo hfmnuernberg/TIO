c454081f399a54980a4f0d0ee38275b5eb75a2e1
Maurice Reichelt <maurice@cultivate.software>
2025-10-02 17:32:03 +0200

    fix: test adjustments

--- lib/l10n/de.dart
+++ lib/l10n/de.dart
@@ -257,7 +257,7 @@ class German extends AppLocalizations {
   String get projectToolList => 'Tool-Liste';
   String get projectToolListEmpty => 'Leere Tool-Liste';
   String get projectTutorialChangeToolOrder =>
-      'Tippe auf das Plus-Symbol, um ein neues Tool hinzuzufügen oder auf das Stift-Symbol um die Tools zu bearbeiten.';
+      'Tippe auf das Plus-Symbol, um ein neues Tool hinzuzufügen oder auf das Stift-Symbol, um die Tools zu bearbeiten.';
   String get projectTutorialEditTitle => 'Tippe hier, um den Projekttitel zu bearbeiten.';
 
   String get projectsAbout => 'Projekte';
