2d195545e5f7b7a2a53a48be4c2a939a597776e2
Maurice Reichelt <maurice@cultivate.software>
2025-10-15 17:44:15 +0200

    formatting

--- test/mocks/audio_player_handler_mock.dart
+++ test/mocks/audio_player_handler_mock.dart
@@ -7,6 +7,7 @@ class AudioPlayerHandlerMock extends Mock {
   void verifyOnPlayingChangeCalled(bool playing) => verify(() => onIsPlayingChange(playing)).called(1);
   void verifyOnPlayingChangeNeverCalled() => verifyNever(() => onIsPlayingChange(any()));
 
-  void verifyOnPlaybackPositionChangeCalled(double position) => verify(() => onPlaybackPositionChange(position)).called(1);
+  void verifyOnPlaybackPositionChangeCalled(double position) =>
+      verify(() => onPlaybackPositionChange(position)).called(1);
   void verifyOnPlaybackPositionChangeNeverCalled() => verifyNever(() => onPlaybackPositionChange(any()));
 }
