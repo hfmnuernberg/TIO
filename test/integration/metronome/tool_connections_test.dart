883dd1dc33d13ca1bfbe3b2c32896847e6510180
Jerome Weiß <jerome@cultivate.team>
2025-10-15 10:49:18 +0200

    test: delete deactivated tests

--- test/integration/metronome/tool_connections_test.dart
+++ test/integration/metronome/tool_connections_test.dart
@@ -40,20 +40,6 @@ void main() {
 
   group('Metronome - connections to other tools', () {
     group('connection to existing tools', () {
-      // deactivated until TIO-264 feature is done
-      // testWidgets('connects media-player', (tester) async {
-      //   await tester.renderScaffold(ProjectPage(goStraightToTool: false, withoutRealProject: false), context.providers);
-      //   await tester.createMetronomeToolInProject();
-      //   await tester.tapAndSettle(find.byTooltip('Add new tool'));
-      //   await tester.createMediaPlayerToolInProject();
-      //
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Metronome 1'));
-      //   await tester.openConnectionDialog();
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Media Player 1'));
-      //
-      //   expect(find.bySemanticsLabel('Media Player 1'), findsOneWidget);
-      // });
-
       testWidgets('connects tuner', (tester) async {
         await tester.renderScaffold(ProjectPage(goStraightToTool: false, withoutRealProject: false), context.providers);
         await tester.createMetronomeToolInProject();
@@ -70,25 +56,6 @@ void main() {
     });
 
     group('connection to new/none existing tools', () {
-      // deactivated until TIO-264 feature is done
-      // testWidgets('adds and connects media-player', (tester) async {
-      //   await tester.renderScaffold(ProjectPage(goStraightToTool: false, withoutRealProject: false), context.providers);
-      //   await tester.createMetronomeToolInProject();
-      //
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Metronome 1'));
-      //   await tester.openConnectionDialog();
-      //
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Media Player'));
-      //   await tester.enterTextAndSettle(find.bySemanticsLabel('Tool title'), 'Media Player 1');
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Submit'));
-      //
-      //   expect(find.bySemanticsLabel('Media Player 1'), findsOneWidget);
-      //
-      //   await tester.tapAndSettle(find.bySemanticsLabel('Back'));
-      //
-      //   expect(tester.withinList(find.bySemanticsLabel('Media Player 1')), findsOneWidget);
-      // });
-
       testWidgets('adds and connects tuner', (tester) async {
         await tester.renderScaffold(ProjectPage(goStraightToTool: false, withoutRealProject: false), context.providers);
         await tester.createMetronomeToolInProject();
