d87d9c7bc2c3132ffd3dc87fd21672be355ee1bf
Maurice Reichelt <maurice@cultivate.software>
2025-10-23 12:09:01 +0200

    split recorder tests

--- test/domain/audio/recorder/recorder_start_test.dart
+++ test/domain/audio/recorder/recorder_start_test.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -14,10 +12,6 @@ void main() {
   late PermissionHandlerMock permissionHandlerMock;
   late RecorderHandlerMock recorderHandlerMock;
   late Recorder recorder;
-
-  setUpAll(() {
-    registerFallbackValue(Permission.microphone);
-  });
 
   setUp(() async {
     resetMocktailState();
@@ -114,52 +108,6 @@ void main() {
       await recorder.stop();
     });
 
-    testWidgets('stops recorder in audio system when stopped', (tester) async {
-      await recorder.start();
-
-      await recorder.stop();
-
-      context.audioSystemMock.verifyMediaPlayerStopRecordingCalled();
-    });
-
-    testWidgets('stops only once', (tester) async {
-      await recorder.start();
-
-      await recorder.stop();
-      await recorder.stop();
-
-      context.audioSystemMock.verifyMediaPlayerStopRecordingCalled();
-    });
-
-    testWidgets('notifies about recording change when stopped', (tester) async {
-      recorder = Recorder(
-        context.audioSystem,
-        context.audioSession,
-        context.wakelock,
-        onIsRecordingChange: recorderHandlerMock.onIsRecordingChange,
-      );
-      await recorder.start();
-      recorderHandlerMock.verifyOnIsRecordingChangeWith(true);
-
-      await recorder.stop();
-      recorderHandlerMock.verifyOnIsRecordingChangeWith(false);
-    });
-
-    testWidgets('unregisters interruption listener on stop', (tester) async {
-      await recorder.start();
-      await recorder.stop();
-
-      context.audioSessionMock.verifyUnregisterInterruptionListenerCalled();
-    });
-
-    testWidgets('allows screen to turn off when stopped', (tester) async {
-      await recorder.start();
-
-      await recorder.stop();
-
-      context.wakelockMock.verifyDisableCalled();
-    });
-
     testWidgets('fails to start recorder when audio system signals failure', (tester) async {
       context.audioSystemMock.mockMediaPlayerStartRecording(false);
 
@@ -174,26 +122,6 @@ void main() {
       await recorder.start();
 
       expect(recorder.isRecording, isFalse);
-    });
-
-    testWidgets('stops recorder anyways when audio system signals failure', (tester) async {
-      await recorder.start();
-
-      expect(recorder.isRecording, isTrue);
-      context.audioSystemMock.mockMediaPlayerStopRecording(false);
-      await recorder.stop();
-
-      expect(recorder.isRecording, isFalse);
-    });
-
-    testWidgets('returns recording samples from audio system', (tester) async {
-      final expectedSamples = Float64List.fromList([0.1, 0.2, 0.3]);
-      context.audioSystemMock.mockMediaPlayerGetRecordingSamples(expectedSamples);
-
-      final result = await recorder.getRecordingSamples();
-
-      expect(result, expectedSamples);
-      context.audioSystemMock.verifyMediaPlayerGetRecordingSamplesCalled();
     });
   });
 }
