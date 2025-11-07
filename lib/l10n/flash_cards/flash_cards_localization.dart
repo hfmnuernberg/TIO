a94316641270fbd8faf7e5093bbee79a2b3e2027
Jerome Weiß <jerome@cultivate.team>
2025-11-07 15:41:32 +0100

    refactor: delete not necessary localization keys

--- lib/l10n/flash_cards/flash_cards_localization.dart
+++ lib/l10n/flash_cards/flash_cards_localization.dart
@@ -1,10 +1,4 @@
 abstract class FlashCardsLocalization {
-  String get descriptionBreakWaterBeauty;
-  String get descriptionBreathOutThreeTimes;
-  String get descriptionCloseEyesReleaseTension;
-  String get descriptionDaysOffCongratulations;
-  String get descriptionMeditateFiveMinBefore;
-
   String flashCardDescriptionById(String id);
 
   String get flashCardTitle;
