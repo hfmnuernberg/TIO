a9a8fd34357c34e0dc2d0888e2d87406144eeace
Jerome Weiß <jerome@cultivate.team>
2025-11-18 17:43:10 +0100

    refactor: simplify flash card implementation

--- lib/services/impl/flash_cards_impl.dart
+++ lib/services/impl/flash_cards_impl.dart
@@ -5,16 +5,12 @@ import 'package:tiomusic/domain/flash_cards/flash_cards_list.dart';
 import 'package:tiomusic/services/flash_cards.dart';
 
 class FlashCardsImpl implements FlashCards {
-  final Random _random;
-
-  FlashCardsImpl({Random? random}) : _random = random ?? Random();
-
   @override
   List<FlashCardModel> load() => List.unmodifiable(flashCards);
 
   @override
   FlashCardModel loadRandom() {
     final cards = load();
-    return cards[_random.nextInt(cards.length)];
+    return cards[Random().nextInt(cards.length)];
   }
 }
