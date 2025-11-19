a93b86d93d8e510cdb798cd9becb9034a5636fc4
Jerome Weiß <jerome@cultivate.team>
2025-11-19 16:14:29 +0100

    feat: show same tip until day ends

--- lib/services/impl/flash_cards_impl.dart
+++ lib/services/impl/flash_cards_impl.dart
@@ -5,15 +5,44 @@ import 'package:tiomusic/domain/flash_cards/flash_cards_list.dart';
 import 'package:tiomusic/models/project_library.dart';
 import 'package:tiomusic/services/flash_cards.dart';
 
+bool isSameDay(DateTime a, DateTime b) => a.year == b.year && a.month == b.month && a.day == b.day;
+
+FlashCardModel? findCardById(List<FlashCardModel> cards, String id) {
+  for (final card in cards) {
+    if (card.id == id) return card;
+  }
+  return null;
+}
+
 class FlashCardsImpl implements FlashCards {
   @override
   List<FlashCardModel> load() => List.unmodifiable(flashCards);
 
   @override
   FlashCardModel loadNext(ProjectLibrary library) {
+    final now = DateTime.now();
     final cards = load();
     final seenCards = library.seenFlashCards;
     final unseenCards = <FlashCardModel>[];
+
+    SeenFlashCard? todaysEntry;
+
+    for (var i = seenCards.length - 1; i >= 0; i--) {
+      final card = seenCards[i];
+      if (isSameDay(card.seenAt, now)) {
+        todaysEntry = card;
+        break;
+      }
+    }
+
+    if (todaysEntry != null) {
+      final todayCard = findCardById(cards, todaysEntry.id);
+
+      if (todayCard != null) {
+        return todayCard;
+      }
+    }
+
 
     if (seenCards.length >= cards.length) {
       seenCards.clear();
@@ -21,7 +50,9 @@ class FlashCardsImpl implements FlashCards {
 
     for (final card in cards) {
       final alreadySeen = seenCards.any((seen) => seen.id == card.id);
-      if (!alreadySeen) unseenCards.add(card);
+      if (!alreadySeen) {
+        unseenCards.add(card);
+      }
     }
 
     if (unseenCards.isEmpty) {
@@ -31,7 +62,7 @@ class FlashCardsImpl implements FlashCards {
 
     final currentCard = unseenCards[Random().nextInt(unseenCards.length)];
 
-    seenCards.add(SeenFlashCard(id: currentCard.id, seenAt: DateTime.now()));
+    seenCards.add(SeenFlashCard(id: currentCard.id, seenAt: now));
 
     return currentCard;
   }
