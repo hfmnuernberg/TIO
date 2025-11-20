fd11ae85e352aa5337a980728b9228ed5cc585b2
Jerome Weiß <jerome@cultivate.team>
2025-11-20 12:00:18 +0100

    test: move tutorials helper in utils file

--- test/integration/tutorials/tutorials_test.dart
+++ test/integration/tutorials/tutorials_test.dart
@@ -5,73 +5,7 @@ import 'package:tiomusic/pages/projects_page/projects_page.dart';
 import '../../utils/action_utils.dart';
 import '../../utils/render_utils.dart';
 import '../../utils/test_context.dart';
-
-extension WidgetTesterPumpExtension on WidgetTester {
-  Future<void> createProjectWithoutTool(String title) async {
-    await tapAndSettle(find.byTooltip('Add new project'));
-    await enterTextAndSettle(find.bySemanticsLabel('New project'), title);
-    await tapAndSettle(find.bySemanticsLabel('Submit'));
-  }
-
-  Future<void> createAndOpenTool(String tool) async {
-    await tapAndSettle(find.bySemanticsLabel(tool));
-    await enterTextAndSettle(find.bySemanticsLabel('Tool title'), '$tool 1');
-    await tapAndSettle(find.bySemanticsLabel('Submit'));
-  }
-
-  Future<void> createAndOpenQuickTool(String tool) async {
-    await tapAndSettle(find.bySemanticsLabel(tool));
-  }
-
-  Future<void> completeInitialTutorial() async {
-    await waitForTutorialNext();
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-  }
-
-  Future<void> completeParentToolTutorial() async {
-    await waitForTutorialNext();
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-  }
-
-  Future<void> completeProjectTutorial() async {
-    await waitForTutorialNext();
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-  }
-
-  Future<void> completeTunerTutorial() async {
-    await waitForTutorialNext();
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-  }
-
-  Future<void> completeTextTutorial() async {
-    await waitForTutorialNext();
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-    await tapAndSettle(find.bySemanticsLabel('Next'));
-  }
-
-  Future<void> goBackAndSaveQuickToolInNewProject() async {
-    await tapAndSettle(find.bySemanticsLabel('Back'));
-    await tapAndSettle(find.bySemanticsLabel('Yes'));
-    await tapAndSettle(find.bySemanticsLabel('Save in new project'));
-    await tapAndSettle(find.bySemanticsLabel('Submit'));
-  }
-
-  Future<void> waitForTutorialNext({Duration timeout = const Duration(seconds: 5)}) async {
-    final next = find.bySemanticsLabel('Next');
-    const step = Duration(milliseconds: 100);
-    var waited = Duration.zero;
-    while (next.evaluate().isEmpty && waited < timeout) {
-      await pump(step);
-      waited += step;
-    }
-  }
-}
+import 'tutorials_utils.dart';
 
 void main() {
   late TestContext context;
@@ -83,9 +17,7 @@ void main() {
     await context.init(dismissTutorials: false);
   });
 
-  tearDown(() {
-    WidgetsBinding.instance.resetEpoch();
-  });
+  tearDown(WidgetsBinding.instance.resetEpoch);
 
   testWidgets('shows projects tutorial initially', (tester) async {
     await tester.renderScaffold(ProjectsPage(), context.providers);
