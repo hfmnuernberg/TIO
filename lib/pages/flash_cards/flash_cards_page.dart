4f24167d3ff7cceeaa02fc9c1f53d7ea736b106e
Jerome Weiß <jerome@cultivate.team>
2025-11-24 18:28:30 +0100

    refactor: extract list in its own file

--- lib/pages/flash_cards/flash_cards_page.dart
+++ lib/pages/flash_cards/flash_cards_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:tiomusic/domain/flash_cards/category.dart';
 import 'package:tiomusic/l10n/app_localizations_extension.dart';
 import 'package:tiomusic/widgets/flash_cards/category_filter_button.dart';
-import 'package:tiomusic/services/flash_cards.dart';
 import 'package:tiomusic/util/color_constants.dart';
-import 'package:tiomusic/widgets/flash_cards/flash_card.dart';
+import 'package:tiomusic/widgets/flash_cards/flash_cards_list.dart';
 
 class FlashCardsPage extends StatefulWidget {
   const FlashCardsPage({super.key});
@@ -42,33 +40,10 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
                 ),
               ),
             ),
-            Expanded(child: _FlashCardsList(categoryFilter: selectedCategory)),
+            Expanded(child: FlashCardsList(categoryFilter: selectedCategory)),
           ],
         ),
       ),
-    );
-  }
-}
-
-class _FlashCardsList extends StatelessWidget {
-  final FlashCardCategory? categoryFilter;
-
-  const _FlashCardsList({this.categoryFilter});
-
-  @override
-  Widget build(BuildContext context) {
-    final cards = context.read<FlashCards>().getAll();
-    final filteredCards = categoryFilter == null
-        ? cards
-        : cards.where((card) => card.category == categoryFilter).toList();
-    final bottomInset = MediaQuery.of(context).padding.bottom;
-
-    return ListView.separated(
-      padding: EdgeInsets.fromLTRB(16, 16, 16, 32 + bottomInset),
-      itemBuilder: (_, i) =>
-          FlashCard(category: filteredCards[i].category, description: filteredCards[i].description(context.l10n)),
-      separatorBuilder: (_, _) => const SizedBox(height: 8),
-      itemCount: filteredCards.length,
     );
   }
 }
--- lib/widgets/flash_cards/flash_cards_list.dart
+++ lib/widgets/flash_cards/flash_cards_list.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiomusic/domain/flash_cards/category.dart';
+import 'package:tiomusic/l10n/app_localizations_extension.dart';
+import 'package:tiomusic/services/flash_cards.dart';
+import 'package:tiomusic/widgets/flash_cards/flash_card.dart';
 
+class FlashCardsList extends StatelessWidget {
+  final FlashCardCategory? categoryFilter;
+
+  const FlashCardsList({super.key, this.categoryFilter});
+
+  @override
+  Widget build(BuildContext context) {
+    final cards = context.read<FlashCards>().getAll();
+    final filteredCards = categoryFilter == null
+        ? cards
+        : cards.where((card) => card.category == categoryFilter).toList();
+    final bottomInset = MediaQuery.of(context).padding.bottom;
+
+    return ListView.separated(
+      padding: EdgeInsets.fromLTRB(16, 16, 16, 32 + bottomInset),
+      itemBuilder: (_, i) =>
+          FlashCard(category: filteredCards[i].category, description: filteredCards[i].description(context.l10n)),
+      separatorBuilder: (_, _) => const SizedBox(height: 8),
+      itemCount: filteredCards.length,
+    );
+  }
+}
