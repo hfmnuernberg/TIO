edf38354520609023991686d33bfa6d394f48cdc
Maurice Reichelt <maurice@cultivate.software>
2025-11-25 10:02:52 +0100

    extract waveform into separate wrapper around waveform_visualizer

--- lib/pages/media_player/markers/edit_markers_page.dart
+++ lib/pages/media_player/markers/edit_markers_page.dart
@@ -6,7 +6,7 @@ import 'package:tiomusic/l10n/app_localizations_extension.dart';
 import 'package:tiomusic/models/blocks/media_player_block.dart';
 import 'package:tiomusic/models/project_library.dart';
 import 'package:tiomusic/pages/media_player/markers/markers.dart';
-import 'package:tiomusic/pages/media_player/waveform_visualizer.dart';
+import 'package:tiomusic/pages/media_player/waveform/waveform.dart';
 import 'package:tiomusic/pages/parent_tool/parent_setting_page.dart';
 import 'package:tiomusic/services/project_repository.dart';
 import 'package:tiomusic/util/color_constants.dart';
@@ -29,43 +29,17 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
   MediaPlayerBlock get block => widget.mediaPlayerBlock;
   Player get player => widget.player;
 
-  final GlobalKey _waveKey = GlobalKey();
-  late WaveformVisualizer _waveformVisualizer;
-  double _waveFormWidth = 0;
+  final List<double> _markerPositions = List.empty(growable: true);
   final double _waveFormHeight = 200;
   double _sliderValue = 0;
+  Duration _positionDuration = Duration.zero;
+  double? _selectedMarkerPosition;
+  bool get _hasSelectedMarker => _selectedMarkerPosition != null;
+  bool _wasPlayingBeforeSliderDrag = false;
+  double _paintedWaveWidth = 0;
 
   late final OnPlaybackPositionChange _playbackListener;
-
-  Duration _positionDuration = Duration.zero;
-
-  double? _selectedMarkerPosition;
-
-  final List<double> _markerPositions = List.empty(growable: true);
-
-  bool get _hasSelectedMarker => _selectedMarkerPosition != null;
-
-  double get _paintedWaveWidth {
-    final buildContext = _waveKey.currentContext;
-    if (buildContext == null) return _waveFormWidth;
-    final renderObject = buildContext.findRenderObject();
-    if (renderObject is RenderBox) return renderObject.size.width;
-    return _waveFormWidth;
-  }
-
   late bool _originalRepeat;
-
-  bool _wasPlayingBeforeSliderDrag = false;
-
-  double _viewStart = 0;
-  double _viewEnd = 1;
-
-  static const double _minSpan = 1 / 10;
-  static const double _maxSpan = 1;
-
-  double _initialViewStart = 0;
-  double _initialViewEnd = 1;
-  double _pinchFocalPosition = 0.5;
 
   @override
   void initState() {
@@ -77,32 +51,7 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
     block.markerPositions.forEach(_markerPositions.add);
 
     _positionDuration = player.fileDuration * _sliderValue;
-
-    _waveformVisualizer = WaveformVisualizer(
-      0,
-      block.rangeStart,
-      block.rangeEnd,
-      widget.rmsValues,
-      viewStart: _viewStart,
-      viewEnd: _viewEnd,
-    );
-
     _syncPlayerMarkers();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      _waveFormWidth = MediaQuery.of(context).size.width - (TIOMusicParams.edgeInset * 2);
-
-      setState(
-        () => _waveformVisualizer = WaveformVisualizer(
-          0,
-          block.rangeStart,
-          block.rangeEnd,
-          widget.rmsValues,
-          viewStart: _viewStart,
-          viewEnd: _viewEnd,
-        ),
-      );
-    });
 
     _playbackListener = _handlePlaybackPositionChange;
     player.addOnPlaybackPositionChangeListener(_playbackListener);
@@ -116,22 +65,12 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
     super.dispose();
   }
 
-  void _syncPlayerMarkers() {
-    player.markers.positions = _markerPositions;
-  }
+  void _syncPlayerMarkers() => player.markers.positions = _markerPositions;
 
   void _updateUiForPosition(double position) {
     final clamped = position.clamp(0.0, 1.0);
     _sliderValue = clamped;
     _positionDuration = player.fileDuration * clamped;
-    _waveformVisualizer = WaveformVisualizer(
-      clamped,
-      block.rangeStart,
-      block.rangeEnd,
-      widget.rmsValues,
-      viewStart: _viewStart,
-      viewEnd: _viewEnd,
-    );
   }
 
   void _handlePlaybackPositionChange(double position) {
@@ -155,11 +94,9 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
     setState(() {});
   }
 
-  Future<void> _updatePositionFromWave(double tapX) async {
-    final double snappedRelativePosition = _calculateSnappedRelativePosition(tapX);
+  void _handleWaveformPositionChange(double snappedRelativePosition) {
     final double? markerPosition = _findMarkerNear(snappedRelativePosition);
-
-    await _seekToPosition(snappedRelativePosition, updateSelectedMarker: true, selectedMarkerPosition: markerPosition);
+    _seekToPosition(snappedRelativePosition, updateSelectedMarker: true, selectedMarkerPosition: markerPosition);
   }
 
   Future<void> _seekToPosition(
@@ -186,26 +123,6 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
     }
     if (!mounted) return;
     setState(() {});
-  }
-
-  double _calculateSnappedRelativePosition(double tapX) {
-    final int totalBins = widget.rmsValues.length;
-    if (totalBins <= 1) return 0;
-
-    final double clampedStart = _viewStart.clamp(0.0, 1.0);
-    final double clampedEnd = _viewEnd.clamp(clampedStart, 1.0);
-
-    final int firstVisibleIndex = (clampedStart * (totalBins - 1)).round().clamp(0, totalBins - 1);
-    final int lastVisibleIndex = (clampedEnd * (totalBins - 1)).round().clamp(firstVisibleIndex, totalBins - 1);
-
-    final int visibleBins = (lastVisibleIndex - firstVisibleIndex + 1).clamp(1, totalBins);
-
-    final int localIndex = WaveformVisualizer.indexForX(tapX, _paintedWaveWidth, visibleBins);
-
-    final double localFraction = visibleBins > 1 ? localIndex / (visibleBins - 1) : 0.0;
-
-    final double span = (clampedEnd - clampedStart).clamp(_minSpan, _maxSpan);
-    return clampedStart + localFraction * span;
   }
 
   double? _findMarkerNear(double snappedRelativePosition) {
@@ -252,58 +169,17 @@ class _EditMarkersPageState extends State<EditMarkersPage> {
             height: _waveFormHeight,
             child: Stack(
               children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(TIOMusicParams.edgeInset, 0, TIOMusicParams.edgeInset, 0),
-                  child: GestureDetector(
-                    onTapUp: (details) => _updatePositionFromWave(details.localPosition.dx),
-                    onScaleStart: (details) {
-                      if (details.pointerCount < 2) return;
-                      _initialViewStart = _viewStart;
-                      _initialViewEnd = _viewEnd;
-
-                      final dx = details.localFocalPoint.dx;
-                      _pinchFocalPosition = _calculateSnappedRelativePosition(dx);
-                    },
-                    onScaleUpdate: (details) {
-                      if (details.pointerCount < 2) return;
-
-                      final double scale = details.scale;
-                      final double initialSpan = _initialViewEnd - _initialViewStart;
-
-                      double newSpan = (initialSpan / scale).clamp(_minSpan, _maxSpan);
-
-                      double center = _pinchFocalPosition;
-                      double start = center - newSpan / 2;
-                      double end = center + newSpan / 2;
-
-                      if (start < 0) {
-                        end -= start;
-                        start = 0;
-                      }
-                      if (end > 1) {
-                        start -= end - 1;
-                        end = 1;
-                      }
-
-                      setState(() {
-                        _viewStart = start;
-                        _viewEnd = end;
-                        _waveformVisualizer = WaveformVisualizer(
-                          _sliderValue,
-                          block.rangeStart,
-                          block.rangeEnd,
-                          widget.rmsValues,
-                          viewStart: _viewStart,
-                          viewEnd: _viewEnd,
-                        );
-                      });
-                    },
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: _waveFormHeight,
-                      child: CustomPaint(key: _waveKey, painter: _waveformVisualizer),
-                    ),
-                  ),
+                Waveform(
+                  rmsValues: widget.rmsValues,
+                  playbackPosition: _sliderValue,
+                  rangeStart: block.rangeStart,
+                  rangeEnd: block.rangeEnd,
+                  height: _waveFormHeight,
+                  onPositionChange: _handleWaveformPositionChange,
+                  onPaintedWidthChange: (width) {
+                    if (width == _paintedWaveWidth) return;
+                    setState(() => _paintedWaveWidth = width);
+                  },
                 ),
                 Markers(
                   rmsValues: widget.rmsValues,
---
+++ lib/pages/media_player/waveform/waveform.dart
@@ -0,0 +1,169 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:tiomusic/pages/media_player/waveform_visualizer.dart';
+import 'package:tiomusic/util/constants.dart';
+
+class Waveform extends StatefulWidget {
+  final Float32List rmsValues;
+  final double playbackPosition;
+  final double rangeStart;
+  final double rangeEnd;
+  final double height;
+  final EdgeInsetsGeometry padding;
+  final ValueChanged<double> onPositionChange;
+  final ValueChanged<double>? onPaintedWidthChange;
+
+  const Waveform({
+    super.key,
+    required this.rmsValues,
+    required this.playbackPosition,
+    required this.rangeStart,
+    required this.rangeEnd,
+    required this.height,
+    required this.onPositionChange,
+    this.onPaintedWidthChange,
+    this.padding = const EdgeInsets.fromLTRB(TIOMusicParams.edgeInset, 0, TIOMusicParams.edgeInset, 0),
+  });
+
+  @override
+  State<Waveform> createState() => _WaveformState();
+}
+
+class _WaveformState extends State<Waveform> {
+  final GlobalKey _waveKey = GlobalKey();
+  late WaveformVisualizer _waveformVisualizer;
+
+  double _viewStart = 0;
+  double _viewEnd = 1;
+
+  static const double _minSpan = 1 / 10;
+  static const double _maxSpan = 1;
+
+  double _initialViewStart = 0;
+  double _initialViewEnd = 1;
+  double _pinchFocalPosition = 0.5;
+
+  double get _paintedWaveWidth {
+    final buildContext = _waveKey.currentContext;
+    if (buildContext == null) return 0;
+    final renderObject = buildContext.findRenderObject();
+    if (renderObject is RenderBox) return renderObject.size.width;
+    return 0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _rebuildVisualizer();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _notifyPaintedWidth());
+  }
+
+  @override
+  void didUpdateWidget(covariant Waveform oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.rmsValues != widget.rmsValues ||
+        oldWidget.playbackPosition != widget.playbackPosition ||
+        oldWidget.rangeStart != widget.rangeStart ||
+        oldWidget.rangeEnd != widget.rangeEnd) {
+      _rebuildVisualizer();
+      WidgetsBinding.instance.addPostFrameCallback((_) => _notifyPaintedWidth());
+    }
+  }
+
+  void _rebuildVisualizer() {
+    _waveformVisualizer = WaveformVisualizer(
+      widget.playbackPosition,
+      widget.rangeStart,
+      widget.rangeEnd,
+      widget.rmsValues,
+      viewStart: _viewStart,
+      viewEnd: _viewEnd,
+    );
+  }
+
+  void _notifyPaintedWidth() {
+    if (widget.onPaintedWidthChange == null) return;
+    final width = _paintedWaveWidth;
+    if (width > 0) widget.onPaintedWidthChange!(width);
+  }
+
+  void _handleTap(Offset localPosition) {
+    final snappedRelative = _calculateSnappedRelativePosition(localPosition.dx);
+    widget.onPositionChange(snappedRelative);
+  }
+
+  double _calculateSnappedRelativePosition(double tapX) {
+    final int totalBins = widget.rmsValues.length;
+    if (totalBins <= 1) return 0;
+
+    final double clampedStart = _viewStart.clamp(0.0, 1.0);
+    final double clampedEnd = _viewEnd.clamp(clampedStart, 1.0);
+
+    final int firstVisibleIndex = (clampedStart * (totalBins - 1)).round().clamp(0, totalBins - 1);
+    final int lastVisibleIndex = (clampedEnd * (totalBins - 1)).round().clamp(firstVisibleIndex, totalBins - 1);
+
+    final int visibleBins = (lastVisibleIndex - firstVisibleIndex + 1).clamp(1, totalBins);
+
+    final double width = _paintedWaveWidth;
+    if (width <= 0) return clampedStart;
+
+    final int localIndex = WaveformVisualizer.indexForX(tapX, width, visibleBins);
+
+    final double localFraction = visibleBins > 1 ? localIndex / (visibleBins - 1) : 0.0;
+
+    final double span = (clampedEnd - clampedStart).clamp(_minSpan, _maxSpan);
+    return clampedStart + localFraction * span;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: widget.padding,
+      child: GestureDetector(
+        onTapUp: (details) => _handleTap(details.localPosition),
+        onScaleStart: (details) {
+          if (details.pointerCount < 2) return;
+          _initialViewStart = _viewStart;
+          _initialViewEnd = _viewEnd;
+
+          final dx = details.localFocalPoint.dx;
+          _pinchFocalPosition = _calculateSnappedRelativePosition(dx);
+        },
+        onScaleUpdate: (details) {
+          if (details.pointerCount < 2) return;
+
+          final double scale = details.scale;
+          final double initialSpan = _initialViewEnd - _initialViewStart;
+
+          double newSpan = (initialSpan / scale).clamp(_minSpan, _maxSpan);
+
+          double center = _pinchFocalPosition;
+          double start = center - newSpan / 2;
+          double end = center + newSpan / 2;
+
+          if (start < 0) {
+            end -= start;
+            start = 0;
+          }
+          if (end > 1) {
+            start -= end - 1;
+            end = 1;
+          }
+
+          setState(() {
+            _viewStart = start;
+            _viewEnd = end;
+            _rebuildVisualizer();
+          });
+        },
+        child: SizedBox(
+          width: double.infinity,
+          height: widget.height,
+          child: CustomPaint(key: _waveKey, painter: _waveformVisualizer),
+        ),
+      ),
+    );
+  }
+}
