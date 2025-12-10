c67a90c35f792f942caf7f25e4e063ebba0feebe
Maurice Reichelt <maurice@cultivate.software>
2025-12-10 13:13:12 +0100

    get rid of range slider

--- lib/pages/media_player/set_trim.dart
+++ lib/pages/media_player/set_trim.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:tiomusic/l10n/app_localizations_extension.dart';
 import 'package:tiomusic/pages/media_player/markers/waveform.dart';
 import 'package:tiomusic/pages/parent_tool/parent_setting_page.dart';
-import 'package:tiomusic/util/color_constants.dart';
-import 'package:tiomusic/util/constants/constants.dart';
 
 const defaultStart = 0.0;
 const defaultEnd = 1.0;
@@ -37,23 +35,15 @@ class SetTrim extends StatefulWidget {
 class _SetTrimState extends State<SetTrim> {
   late RangeValues _rangeValues;
 
-  Duration _rangeStartDuration = Duration.zero;
-  Duration _rangeEndDuration = Duration.zero;
-
   @override
   void initState() {
     super.initState();
-
     _rangeValues = RangeValues(widget.initialStart, widget.initialEnd);
-    _rangeStartDuration = widget.fileDuration * _rangeValues.start;
-    _rangeEndDuration = widget.fileDuration * _rangeValues.end;
   }
 
   Future<void> _handleChange(RangeValues values) async {
     setState(() {
       _rangeValues = values;
-      _rangeStartDuration = widget.fileDuration * _rangeValues.start;
-      _rangeEndDuration = widget.fileDuration * _rangeValues.end;
     });
     if (_rangeValues.start < _rangeValues.end) {
       await widget.onChange(_rangeValues.start, _rangeValues.end);
@@ -109,7 +99,7 @@ class _SetTrimState extends State<SetTrim> {
       reset: _handleReset,
       cancel: _handleCancel,
       customWidget: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Waveform(
             rmsValues: widget.rmsValues,
@@ -121,30 +111,6 @@ class _SetTrimState extends State<SetTrim> {
             selectedMarkerPosition: null,
             onPositionChange: _handleWaveformPositionChange,
             onZoomChanged: (_, __) {},
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: TIOMusicParams.edgeInset),
-            child: RangeSlider(
-              values: _rangeValues,
-              inactiveColor: ColorTheme.primary80,
-              divisions: 1000,
-              labels: RangeLabels(
-                l10n.formatDurationWithMillis(_rangeStartDuration),
-                l10n.formatDurationWithMillis(_rangeEndDuration),
-              ),
-              onChanged: (values) async {
-                var start = values.start;
-                var end = values.end;
-                if (start == end) {
-                  end = end + 0.001;
-                  if (end > 1.0) {
-                    end = 1.0;
-                    start = 0.999;
-                  }
-                }
-                await _handleChange(RangeValues(start, end));
-              },
-            ),
           ),
         ],
       ),
