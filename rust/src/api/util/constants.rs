e2edcadbd51c4f634d2dab3981e243265f03f378
David Bieder <davidbieder@users.noreply.github.com>
2026-04-03 15:46:30 +0200

    fix: TIO-107: change pitch shift oversampling (#724)
    **Are there specific parts that the reviewer should look out for?**
    
    - [x] no
    - [ ] yes (please specify)
    
    **Any other comments?** _(e.g., unrelated minor changes piggybacking
    this PR)_
    
    - [x] no
    - [ ] yes (please specify)
    
    **Helpful screenshots?**
    
    - [x] no
    - [ ] yes

--- rust/src/api/util/constants.rs
+++ rust/src/api/util/constants.rs
@@ -7,8 +7,6 @@ pub const NUM_CHANNELS: usize = 1;
 pub const AUDIO_STREAM_CREATE_TIMEOUT_SECONDS: u64 = 10;
 
 pub const SAMPLE_RATE: usize = 44100;
-pub const SAMPLE_RATE_HALF: i32 = 22050;
-pub const SAMPLE_RATE_IN_KHZ: f32 = 44.1;
 
 pub static OUTPUT_SAMPLE_RATE: Mutex<usize> = Mutex::new(SAMPLE_RATE);
 pub static INPUT_SAMPLE_RATE: Mutex<usize> = Mutex::new(SAMPLE_RATE);
@@ -18,7 +16,7 @@ pub static INPUT_SAMPLE_RATE: Mutex<usize> = Mutex::new(SAMPLE_RATE);
 pub const PITCH_SHIFT_BUFFER_SIZE: usize = 64;
 pub const MEDIA_PLAYER_PLAYBACK_MAX_BUFFERING: usize = 2048 * 2 + PITCH_SHIFT_BUFFER_SIZE;
 pub const MEDIA_PLAYER_PLAYBACK_MIN_BUFFERING: usize = 1024;
-pub const PITCH_SHIFT_OVERSAMPLING: usize = 4;
+pub const PITCH_SHIFT_OVERSAMPLING: usize = 8;
 pub const PITCH_SHIFT_WINDOW_DUR_MILLIS: usize = 70;
 
 // tuner
